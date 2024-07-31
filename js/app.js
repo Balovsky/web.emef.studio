c937a8bda9b9651a1e20f53e9031b95fc2c62171
Balovsky <mariuszfranik98@gmail.com>
2024-07-31 22:13:38 +0200

    added scroll arrow to top, fix mobile hamburder menu response

--- js/app.js
+++ js/app.js
@@ -12,4 +12,32 @@ document.addEventListener('DOMContentLoaded', function() {
         navContainer.style.position = 'relative'
     }
 })
+
+const upArrow = document.querySelector('.arrowUp');
+
+    // Funkcja, która pokazuje lub ukrywa przycisk w zależności od przewinięcia
+    function toggleUpArrow() {
+        if (window.scrollY > 100) {
+            upArrow.classList.add('show');
+        } else {
+            upArrow.classList.remove('show');
+        }
+    }
+
+    // Funkcja, która przewija stronę do góry po kliknięciu
+    function scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+
+    // Nasłuchujemy na zdarzenie scroll i kliknięcie
+    window.addEventListener('scroll', toggleUpArrow);
+    upArrow.addEventListener('click', scrollToTop);
+
+    // Inicjalne sprawdzenie pozycji przewijania przy załadowaniu strony
+    toggleUpArrow();
+
+
 });
